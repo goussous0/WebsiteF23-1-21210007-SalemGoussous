e158887de920eb67a354939c8e9f36ad5bd0f5e0
goussous0 <sgoussous0@gmail.com>
2023-01-05 13:35:00 +0300

    adding all files

---
+++ no_bootstrap/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="/static/css/styles.css">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/img/favicon-16x16.png">
+        <ul class="nav_lst" >
+            <li class="nav_elm">
+                <img class="logo" src="/static/img/logo.svg">
+            </li>
+            <li class="nav_elm"><a class="item" href="/">About Us</a></li>
+            <li class="nav_elm"><a class="item" href="/">Menu</a></li>
+            <li class="nav_elm"><a class="item" href="/">Cart</a></li>
+            <li class="nav_elm"><a class="item" href="/">Contact Us</a></li>
+        </ul>  
+    </head>
+    <body>
+        <div class="card mt-1"> <!-- mt stands for margin top with 1rem-->
+            <h4 class="card-header" style="margin:0 auto;font-size: 5rem;">Welcome to 0xDEADBEEF</h4>
+            <div class="card-bg">
+                <img src="/static/img/Ceaser salad made from circut componants.png" alt='Ceaser salad made from circut componants'> 
+            </div>
+        </div>
+
+    </body>
+</html>
